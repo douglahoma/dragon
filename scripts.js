631551cf4572d62cfb966fb566cc01f0b52f72cf
Douglas <wdmdavison@icloud.com>
2023-08-18 02:40:56 -0500

    push for safekeeping; lots of edits to product

--- scripts.js
+++ scripts.js
@@ -25,13 +25,32 @@ if ( document.URL.includes("product.html") ){
     const docRef = db.collection('products').doc(`${id}`);
 
     docRef.get().then((doc) => {
+        // first we'll make sure the doc is there!
         if (doc.exists) {
+            // next we'll make a reference to the data object:
             const data = doc.data();
+            // and next log it to the console for fun:
             console.log(`name is ${data.name} and description is ${data.description}`);
+            // NOW WE START UPDATING THE PRODCT PAGE, yee-haw!
+            // First, the product name goes into main header:
             document.getElementById("productheader").innerHTML = `${data.name}`;
+            // Now let's give it a description so nobody rushes to conclusions:
             document.getElementById("productdescription").innerHTML = `${data.description}`;
+            // Thirdly, the most important thing: the price! (a number)
             document.getElementById("productprice").innerHTML =`${data.price}`;
+            // And, since each product has at least one image, we can definitely safely populate "image1", our main image
             document.getElementById("image1").src=`${data.img1}`;
+            // Next we will check for some more images! Shirts have 6, and they are as such:
+            // img1 : an old man who is young at heart
+            // img2 : a cool guy indoors with a dragon wall
+            // img3 : a millenial hiker with a beard
+            // img4 : a kind-looking lady
+            // img5 : a really cool hip guy with a jacket
+            // img6 : a sideways-oriented cool young dude
+            //
+            // re: home goods, some products (like pillows and mugs!) only have a main image, img1, 
+            // while others (like art and boxes!) have 3.
+            // In conclusion, it can vary! So we are careful to check.
             if (data.img2) {
                 document.getElementById("image2").src = `${data.img2}`;
             }
@@ -50,6 +69,7 @@ if ( document.URL.includes("product.html") ){
 
         } else {
             console.log("no document found in the database :(");
+            location.replace('404.html');
         }
     })
     
